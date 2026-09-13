7fe506bd92c9566c819928684a999a67b86220b4
Harshit Singh Rana <harshit070908@gmail.com>
2026-09-13 18:43:49 +0530

    141

---
+++ Easy/1141_LinkedListCycle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast {head};
+        ListNode* slow {head};
+
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(slow == fast){
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+};

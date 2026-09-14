7c1044dd225a73fe9b3372e8a6c4a876366f7149
Harshit Singh Rana <harshit070908@gmail.com>
2026-09-14 20:08:30 +0530

    206

---
+++ Easy/206_ReverseLinkedList.cpp
@@ -0,0 +1,25 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* first = nullptr;
+        ListNode* second = nullptr;
+        ListNode* third = head;
+
+        while(third != nullptr){
+            first = second;
+            second = third;
+            third = third->next;
+            second->next = first;
+        }
+
+        return second;
+    }
+};

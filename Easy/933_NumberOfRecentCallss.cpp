7a733d136477ed31fa2c8ae94d9c71cd997c76dc
Harshit Singh Rana <harshit070908@gmail.com>
2026-09-21 18:01:02 +0530

    933

---
+++ Easy/933_NumberOfRecentCallss.cpp
@@ -0,0 +1,18 @@
+#include <queue>
+
+class RecentCounter {
+private:
+    std::queue<int> q;
+public:
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+        while(t - q.front() > 3000){
+            q.pop();
+        }
+        return q.size();
+    }
+};
